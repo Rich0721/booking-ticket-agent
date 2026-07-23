4c30dc32d6bf0dbfa9f53af3ba83af1c8c2b415d
Rich0721 <zx20125566@gmail.com>
2026-07-23 15:16:02 +0800

    feat(frontend): 實作 Selection 下拉選單 Component 並於 HomePage 展示

--- frontend/src/components/index.ts
+++ frontend/src/components/index.ts
@@ -3,7 +3,12 @@ export { default as IDNumberInput } from "./IDNumberInput/IDNumberInput";
 export { default as DatePicker } from "./DatePicker/DatePicker";
 export { default as Checkbox } from "./Checkbox/Checkbox";
 export { default as TicketNumber } from "./TicketNumber/TicketNumber";
+export { default as Selection } from "./Selection/Selection";
 export type { IIDNumberInputProps } from "../interfaces/IIDNumberInput";
 export type { IDatePickerProps } from "../interfaces/IDatePicker";
 export type { ICheckboxProps, CheckboxOption } from "../interfaces/ICheckbox";
 export type { ITicketNumberProps } from "../interfaces/ITicketNumber";
+export type {
+  ISelectionProps,
+  ISelectionOption,
+} from "../interfaces/ISelection";
